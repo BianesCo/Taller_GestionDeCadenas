d4f166d0c62d73199bad1f00461c680235fc9108
BianesCo <bibian.corredor@gmail.com>
2023-08-01 19:46:18 -0500

    actualizar

--- src/presenter/Presenter.java
+++ src/presenter/Presenter.java
@@ -2,6 +2,7 @@ package presenter;
 
 
 import model.ETypeProduct;
+import model.Product;
 import model.ServiceSale;
 
 public class Presenter {
@@ -11,17 +12,20 @@ public class Presenter {
 	public Presenter() {
 	}
 	
-	public double addSale(String name, double value, int stock, boolean iva,ETypeProduct eTypeProduct,int cont ) {
-//		if(eTypeProduct == true ) {
-//			return 1.0;
-//		}
 	
-		return 0.0;
+	
+	public double addSale(String name, double value, int stock, boolean iva,ETypeProduct eTypeProduct,int cont ) throws Exception {
+		
+		Product product = new Product(name, value, stock, iva, eTypeProduct);
+		
+		ServiceSale serviceSale = new ServiceSale(product, cont);
+		
+		return value;
 	}
 	public String showSale() {
 		
 		return "el total de la compra /n"+serviceSale.getSale().getTotalSale()+" total agrabado /n "+ serviceSale.getSale().calcIva() ; 
 	}
-	
+
 
 }
